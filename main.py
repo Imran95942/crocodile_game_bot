0b9c1a2c8b571be6c4f5c81103f95dfb4f9c39f8
mehmetalisa <83994095+mehmetalisa@users.noreply.github.com>
2021-09-08 21:23:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -261,7 +261,7 @@ def main():
 
     # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('kelime', kelime)],
+        entry_points=[CommandHandler(kelime)],
 
         states={
             CHOOSING_PLAYER: [CallbackQueryHandler(next_player, pattern="^next_player$"),
